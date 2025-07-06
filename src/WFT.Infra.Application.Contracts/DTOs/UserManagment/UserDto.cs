ee3f319719ca96a821b1ea8dcab6b421bbfc7c6e
Akarimi1989 <a.karimi@danesh.ai>
2025-07-06 22:29:57 +0330

    allow null Roles

--- src/WFT.Infra.Application.Contracts/DTOs/UserManagment/UserDto.cs
+++ src/WFT.Infra.Application.Contracts/DTOs/UserManagment/UserDto.cs
@@ -8,7 +8,7 @@
         public string? LastName { get; set; }
         public bool IsActive { get; set; } = true;
         public DateTime? LastLoginAt { get; set; }
-        public List<string> Roles { get; set; }
+        public List<string>? Roles { get; set; }
 
     }
 }
